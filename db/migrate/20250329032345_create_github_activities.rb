9e6dd52263058032b5261525c775f1298e3ff841
Simple0109 <p.cscscs@icloud.com>
2025-03-29 12:34:19 +0900

    GithubActivityモデルとマイグレーションを定義

--- db/migrate/20250329032345_create_github_activities.rb
+++ db/migrate/20250329032345_create_github_activities.rb
@@ -1,7 +1,18 @@
 class CreateGithubActivities < ActiveRecord::Migration[7.2]
   def change
     create_table :github_activities do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :activity_type, null: false
+      t.string :github_id, null: false
+      t.string :repository_name
+      t.string :repository_url
+      t.text :description
+      t.string :url
+      t.datetime :activity_date
+
       t.timestamps
     end
+
+    add_index :github_activities, :github_id, unique: true
   end
 end
