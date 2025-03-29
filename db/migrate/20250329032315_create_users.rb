bde5abc6ca6e70f2c16c4f8dc579f4fc8eb689d8
Simple0109 <p.cscscs@icloud.com>
2025-03-29 12:30:50 +0900

    Userのマイグレーションを定義

--- db/migrate/20250329032315_create_users.rb
+++ db/migrate/20250329032315_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest
+      t.string :github_username
+      t.string :qiita_username
+
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
