8a5143bb122033938c9fdcc77a84cc9f91085360
Simple0109 <p.cscscs@icloud.com>
2025-03-29 12:33:21 +0900

    Profileモデルとマイグレーションファイルを定義

--- db/migrate/20250329032328_create_profiles.rb
+++ db/migrate/20250329032328_create_profiles.rb
@@ -1,6 +1,13 @@
 class CreateProfiles < ActiveRecord::Migration[7.2]
   def change
     create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.text :bio
+      t.string :avatar_url
+      t.string :location
+      t.string :website
+      
       t.timestamps
     end
   end
