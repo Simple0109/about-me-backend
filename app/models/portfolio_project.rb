263646eccbe4178f434899cd327dd7525183ed05
Simple0109 <p.cscscs@icloud.com>
2025-03-29 12:37:07 +0900

    PortfolioProjectモデルとマイグレーションファイルを定義

--- app/models/portfolio_project.rb
+++ app/models/portfolio_project.rb
@@ -1,2 +1,6 @@
 class PortfolioProject < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- db/migrate/20250329032423_create_portfolio_projects.rb
+++ db/migrate/20250329032423_create_portfolio_projects.rb
@@ -1,6 +1,14 @@
 class CreatePortfolioProjects < ActiveRecord::Migration[7.2]
   def change
     create_table :portfolio_projects do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :image_url
+      t.string :github_url
+      t.string :demo_url
+      t.string :technologies, array: true, default: []
+
       t.timestamps
     end
   end
