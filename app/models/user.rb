f297c681b77e344c1961e05bf2549c1bd087d63d
Simple0109 <p.cscscs@icloud.com>
2025-03-30 17:53:45 +0900

    Userモデルを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_one :profile, dependent: :destroy
   has_many :github_activities, dependent: :destroy
   has_many :qiita_articles, dependent: :destroy
   has_many :portfolio_projects, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
 end
