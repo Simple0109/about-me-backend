5688562623a8ff8912ff7c11e687352f9b231854
Simple0109 <p.cscscs@icloud.com>
2025-03-30 17:21:27 +0900

    不要な記述タイポを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,33 +28,3 @@ class ApplicationController < ActionController::API
     @current_user
   end
 end
-
-class ApplicationController < ActionController::API
-  include ActionController::HttpAuthentication::Token::ControllerMethods
-
-  before_action :authenticate
-
-  protected
-
-  def authenticate
-    authenticate_token || render_unauthorized
-  end
-
-  def authenticate_token
-    authenticate_with_http_token do |token, options|
-      decoded = Auth.decode(token)
-      if decoded.is_a?(Hash) && decoded[:error]
-        false
-      else
-        @current_user = User.find_by(id: decoded[:user_id])
-    end
-  end
-
-  def render_unauthorized
-    render json: { error: I18n.t('auth.error.authentication_failed' )}, status: :unauthorized
-  end
-
-  def current_user
-    @current_user
-  end
-end
